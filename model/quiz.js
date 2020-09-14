b3b6bfe8c0901e41d622ffbbc77bfd3a93a3726a
SBhanushali <shivam.n.bhanushali@gmail.com>
2020-09-14 07:27:25 +0530

    Create model of quiz

---
+++ model/quiz.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+// Setup Schema
+const quizSchema = mongoose.Schema({
+  moduleName: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  question: {
+    type: String,
+    required: true,
+  },
+  inputType: {
+    type: String,
+    required: true,
+  },
+  explanation: {
+    type: String,
+  },
+  options: [
+    {
+      id: mongoose.Schema.Types.ObjectId,
+      value: String,
+      isCorrect: Boolean,
+    },
+  ],
+});
+
+//Export quiz model
+const Quiz = mongoose.model("Quiz", quizSchema);
+module.exports = Quiz;
