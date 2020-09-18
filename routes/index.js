7b095e79fa2d4a7b58a0720443f5665990aa2611
SBhanushali <shivam.n.bhanushali@gmail.com>
2020-09-18 08:07:33 +0530

    Change route naming

--- routes/index.js
+++ routes/index.js
@@ -8,24 +8,17 @@ const {
 //Initialize express router
 const router = require("express").Router();
 
-// Set default API response
-router.get("/", (req, res) => {
-  res.json({
-    status: "API working",
-    message: "Welcome to QuizApp made with love!",
-  });
-});
 // Create questions
-router.post("/create", create);
+router.post("create", create);
 
 // Get all the modules
-router.get("/modules", getAllModules);
+router.get("modules", getAllModules);
 
 // Get all the question from module
-router.get("/questions", getAllQuestionOfModule);
+router.get("questions", getAllQuestionOfModule);
 
 // Check answer
-router.post("/check", checkAnswer);
+router.post("check", checkAnswer);
 
 // Export API routes
 module.exports = router;
